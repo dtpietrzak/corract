e595dfedb1326aef0c95cd431735dd3b39a58f68
dtpietrzak <dtpietrzak@gmail.com>
2025-07-26 19:32:54 -0500

    first implementation of all pages in docs

--- apps/corract-site/src/pages/docs/src/middleware/index.tsx
+++ apps/corract-site/src/pages/docs/src/middleware/index.tsx
@@ -19,7 +19,7 @@ const DocsSrcMiddleware: Page<'/docs/src/middleware', AppPages> = (props) => {
         This directory is essential for implementing custom logic that needs to run before or after the main request handling process. And is used to inject server side data into your components, enabling dynamic rendering based on the request context. Middleware is applied to specific routes as defined in your <Link href={'/docs/app/def'} color={'green.hard'}>app-def.ts</Link>, giving you flexibility in how you handle requests across your application.
       </Text>
       <Text tag={'p'} color={'purple.hard'}>
-        This number: <Code>{docsMiddleware.calls}</Code> represents how many times any docs page has been rendered since the last restart. Try disabling JavaScript in your browser—this count will still increase. That's because the count is tracked in the docs route's middleware, meaning the server renders and delivers the full page regardless of client-side JS. This highlights a core strength of Corract's SSR: complete page rendering without relying on JavaScript. Last render timestamp: <Code>{docsMiddleware.lastCall}</Code>.
+        This number: <Code>{docsMiddleware.calls}</Code> represents how many times any docs page has been rendered since the last restart. Try disabling JavaScript in your browser—this count will still increase. That's because the count is tracked in the docs routes' middleware, meaning the server renders and delivers the full page regardless of client-side JS. This highlights a core strength of Corract's SSR: complete page rendering without relying on JavaScript. Last render timestamp: <Code>{docsMiddleware.lastCall}</Code>.
       </Text>
 
     </>
